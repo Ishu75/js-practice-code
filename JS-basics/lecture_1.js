aa8a3734497ec8377a276e848177d81de6d8124d
Ishu75 <139671150+Ishu75@users.noreply.github.com>
2025-05-11 10:01:43 +0000

    lecture 1

---
+++ JS-basics/lecture_1.js
@@ -0,0 +1,15 @@
+//------------VARIABLES[Let, Const, Var]--------------
+
+
+// As the name suggests, value of const variables can not change
+const accountId = 27453
+
+let accountEmail = "random@gmail.com"
+let accountState // The vale of this variable is undefined
+
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountState, accountCity])
